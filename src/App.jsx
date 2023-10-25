dbfb4e2218bf5a9a4a0ed6e25d2cd2e6c9ba4531
AudunKodehode <audunkodehode@gmail.com>
2023-10-25 17:13:12 +0200

    *

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
         <img src={SPLITTERLOGO} alt="" />
       </div>
       <Splitter />
+      <div className="fem">
+    Challenge by <a href="https://www.frontendmentor.io/solutions/calculatorapp-FBFB67V_0">Frontend Mentor.</a> Coded by <a href="https://github.com/AudunKodehode">AudunKodehode</a>. 
+    </div>
     </>
   );
 }
